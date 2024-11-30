5d699c247ab1229a4b33fc177445cf942ddffe41
Thiago Piffer Lauro <120656984+ThiagoP77@users.noreply.github.com>
2024-11-30 20:19:19 -0300

    Commitando

--- 10-PhP_POO/11-AppSendMail/app_send_mail/processa_envio.php
+++ 10-PhP_POO/11-AppSendMail/app_send_mail/processa_envio.php
@@ -48,8 +48,8 @@
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'thiagopifferlauro@gmail.com';                     //SMTP username
-			$mail->Password   = 'dodt orwk bohr kjgc';                               //SMTP password
+			$mail->Username   = 'meu email';                     //SMTP username
+			$mail->Password   = 'minha senha';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
